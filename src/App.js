4c50b09bba82f09b0a56cebf4bbf009d66c21fd4
Beyza Akbulut <94783712+beyzakb@users.noreply.github.com>
2023-12-08 21:48:42 +0300

    images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import Images from "./Images";
+import { useState } from "react";
 
 function App() {
+
+  const [selectedImg,setSelectedImg] = useState(Images[0])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src={selectedImg} alt="selected" className="selected" />
+      </div>
+      <div className="imgContainer">
+
+        {Images.map((img,index)=>(
+        <img key={index} src={img} alt="random" onClick={()=>setSelectedImg(img)}/>
+
+        ))}
+        
+      </div>
     </div>
   );
 }
---
+++ src/Images.js
@@ -0,0 +1,10 @@
+export default [
+"https://picsum.photos/id/167/250/250",
+"https://picsum.photos/id/17/250/250",
+"https://picsum.photos/id/107/250/250",
+"https://picsum.photos/id/159/250/250",
+"https://picsum.photos/id/19/250/250",
+"https://picsum.photos/id/267/250/250",
+"https://picsum.photos/id/757/250/250",
+"https://picsum.photos/id/377/250/250"
+]
